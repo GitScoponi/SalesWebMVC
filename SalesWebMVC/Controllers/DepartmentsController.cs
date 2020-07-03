59e8e6b2b7b5124200b1bdaa854e995fdc00eb97
Vincius Scoponi <vinicius.scoponi@gmail.com>
2020-07-03 16:58:22 -0300

    Add Controller, View and Model Departmens

---
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> DepartmentsCollections = new List<Department>();
+            DepartmentsCollections.Add(new Department { Id = 1, Nome = "Recursos Humanos" });
+            DepartmentsCollections.Add(new Department { Id = 2, Nome = "Contabilidade" });
+            DepartmentsCollections.Add(new Department { Id = 3, Nome = "Deparatamento Pessoal" });
+            return View(DepartmentsCollections);
+        }
+    }
+}
