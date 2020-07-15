3e87bb4cccb595694303afac1221b59f5b70aa79
Vincius Scoponi <vinicius.scoponi@gmail.com>
2020-07-15 18:21:10 -0300

    Add SalesRecord SimpleSearch

---
+++ SalesWebMVC/Controllers/SalesRecordsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models.Services;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        private readonly SalesRecordsService _salesService;
+
+        public SalesRecordsController(SalesRecordsService salesService)
+        {
+            _salesService = salesService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task< IActionResult > SimpleSearch(DateTime? minDate,DateTime? maxDate)
+        {
+            if(minDate==null)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (maxDate == null)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minData"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxData"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesService.FindByDateAsync(minDate, maxDate);
+            return View(result);
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
